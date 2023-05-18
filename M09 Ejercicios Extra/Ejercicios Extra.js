1949f60fa643d5ab6fb5f6977e579c1be546a05d
jsotelo97 <jsotelo.alatrista@gmail.com>
2023-05-18 14:31:43 -0500

    prueba para pushear

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,11 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+
+   var mArray = [];
+   var sArray = [];
+
+
 }
 
 function numberOfCharacters(string) {
